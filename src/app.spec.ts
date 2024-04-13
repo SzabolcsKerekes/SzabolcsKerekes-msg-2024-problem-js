c754b0767310ebeccdcfa4ac4d86a9681123c0e1
KerekesSzabolcs <kerekesszabi20001120@gmail.com>
2024-04-13 16:39:23 +0300

    test: currency conversions and transfers

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -4,6 +4,7 @@ import { SavingsManagerServiceInstance } from './services/savings-manager.servic
 import { seedInitializer } from './seed/seed-initializer';
 import { MoneyModel } from './domain/money.model';
 import { CurrencyType } from './domain/currency-type.enum';
+import { getConversionRate } from './utils/money.utils';
 
 seedInitializer();
 
@@ -28,7 +29,6 @@ describe('Checking account tests', () => {
         expect(TransactionManagerServiceInstance.checkFunds(checkingAccountB.id).currency).toBe(CurrencyType.RON);
       });
     });
-
 
     describe('Transfer', () => {
       // Transfering money from A to B (CHECKING TO CHECKING)
@@ -57,7 +57,65 @@ describe('Checking account tests', () => {
         expect(TransactionManagerServiceInstance.checkFunds(checkingAccountB.id).currency).toBe(CurrencyType.RON);
       });
     });
-  })
+  });
+
+
+  // --- WITHDRAWAL TESTS --- 
+  describe('Withdrawal tests', () => {
+    describe('Checking funds before withdrawal on C', () => {
+      // Checking account C
+      test('Expected money (amount and currency) on checkingAccountC should be 10EUR', () => {
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountC.id).amount).toBe(10);
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountC.id).currency).toBe(CurrencyType.EUR);
+      });
+    });
+
+    describe('Withdrawal', () => {
+      test('Withdrawing 5EUR from CheckingAccountC', () => {
+        const withdrawal = TransactionManagerServiceInstance.withdraw(
+          checkingAccountC.id,
+          new MoneyModel({ amount: 5, currency: CurrencyType.EUR })
+        );
+        expect(withdrawal.amount.amount).toBe(5);
+        expect(withdrawal.amount.currency).toBe(CurrencyType.EUR); 
+      });
+    });
+
+    describe('Checking funds after withdrawal on C', () => {
+      // Checking account C
+      test('Expected money (amount and currency) on checkingAccountC should be 5EUR', () => {
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountC.id).amount).toBe(5);
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountC.id).currency).toBe(CurrencyType.EUR);
+      });
+    });
+  });
+
+
+  // --- CURRENCY CONVERTING TESTS --- 
+  describe('Currency converting tests', () => {
+
+    describe('Transfering EUR from RON account to RON account', () => {
+      // Checking account C
+      test('Transfering 10EUR from CheckingAccountB to CheckingAccountA', () => {
+        const transaction = TransactionManagerServiceInstance.transfer(
+          checkingAccountA.id,
+          checkingAccountB.id,
+          new MoneyModel({ amount: 10, currency: CurrencyType.EUR })
+        );
+        // multiplying the amount by the exchange rate of EUR/RON to determine how much RON will be sent to the recipient's RON account.
+        expect(transaction.amount.amount).toBe(10*getConversionRate(CurrencyType.EUR, CurrencyType.RON));
+        expect(transaction.amount.currency).toBe(CurrencyType.RON); 
+      });
+      test('Expected money on checkingAccountA should be 50RON-10EUR with the exchange rate of EUR/RON~=4.98 => 0.2RON', () => {
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountA.id).amount).toBe(0.2);
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountA.id).currency).toBe(CurrencyType.RON);
+      });
+      test('Expected money on checkingAccountB should be 350RON+10EUR with the exchange rate of EUR/RON~=4.98 => 399.8RON', () => {
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountB.id).amount).toBe(399.8);
+        expect(TransactionManagerServiceInstance.checkFunds(checkingAccountB.id).currency).toBe(CurrencyType.RON);
+      });
+    });
+  });
 })
 
 
